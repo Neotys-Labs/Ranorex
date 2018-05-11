ac6c4a671d8ad03dd6c557e052e5eb93ce17a8fc
mlasram <mohamed.lasram@neotys.com>
2018-05-11 15:39:43 +0200

    add global parameter to test suite

--- MyNeoloadTest/MyNeoloadTest/AssemblyInfo.cs
+++ MyNeoloadTest/MyNeoloadTest/AssemblyInfo.cs
@@ -9,14 +9,16 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyNeoloadTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Neoload Ranorex")]
+[assembly: AssemblyDescription("Neoload Ranorex Plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyNeoloadTest")]
+[assembly: AssemblyCompany("Neotys")]
+[assembly: AssemblyProduct("Neoload Ranorex")]
 [assembly: AssemblyCopyright("Copyright 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
